568923032dbf1d449d40bf82952cdeb7b79c8567
Chanwoo Kim <chanwoohill@gmail.com>
2016-07-20 05:57:05 +0000

    resubmit max.rb

--- max.rb
+++ max.rb
@@ -1,4 +1,5 @@
 # Find the maximum 
+=begin
 def maximum(arr)
   arr.max
 end
@@ -16,6 +17,20 @@ puts "max of -23, 0, -3 is: #{result}"
 
 result = maximum([6])
 puts "max of just 6 is: #{result}"
+=end
 
+def maximum(arr)
+  maximum = arr[0] 
+  for i in 1..(arr.length-1)
+    if maximum < arr[i]
+      maximum = arr[i] 
+    end
+  end
+  maximum
+end
+
+puts maximum([2, 42, 22, 02])
+puts maximum([])
+puts maximum([-23, -2, -3])
 
 
